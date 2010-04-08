d20c00d590b0d1fdec8ea73b6422f0432a9c0c35
Warren Krewenki <krewenki@gmail.com>
2010-04-07 22:22:46 -0300

    Basic thumbnailer

--- index.php
+++ index.php
@@ -4,6 +4,84 @@ function getTitleFromFileName($filename){
 	return ucwords(strtolower(str_replace('_',' ',array_shift(explode('.',$filename)))));
 }
 
+function thumbnail($inputFileName, $outputFileName, $maxSize = 100){
+        $info = getimagesize($inputFileName);
+
+        $type = isset($info['type']) ? $info['type'] : $info[2];
+
+        // Check support of file type
+        if ( !(imagetypes() & $type) )
+        {
+            // Server does not support file type
+            return false;
+        }
+
+        $width  = isset($info['width'])  ? $info['width']  : $info[0];
+        $height = isset($info['height']) ? $info['height'] : $info[1];
+
+        // Calculate aspect ratio
+        $wRatio = $maxSize / $width;
+        $hRatio = $maxSize / $height;
+
+        // Using imagecreatefromstring will automatically detect the file type
+        $sourceImage = imagecreatefromstring(file_get_contents($inputFileName));
+
+        // Calculate a proportional width and height no larger than the max size.
+        if ( ($width <= $maxSize) && ($height <= $maxSize) )
+        {
+            // Input is smaller than thumbnail, do nothing
+            return $sourceImage;
+        }
+        elseif ( ($wRatio * $height) < $maxSize )
+        {
+            // Image is horizontal
+            $tHeight = ceil($wRatio * $height);
+            $tWidth  = $maxSize;
+        }
+        else
+        {
+            // Image is vertical
+            $tWidth  = ceil($hRatio * $width);
+            $tHeight = $maxSize;
+        }
+
+        $thumb = imagecreatetruecolor($tWidth, $tHeight);
+
+        if ( $sourceImage === false )
+        {
+            // Could not load image
+            return false;
+        }
+
+        // Copy resampled makes a smooth thumbnail
+        imagecopyresampled($thumb, $sourceImage, 0, 0, 0, 0, $tWidth, $tHeight, $width, $height);
+        imagedestroy($sourceImage);
+
+        $ext = strtolower(substr($outputFileName, strrpos($outputFileName, '.')));
+
+        switch ( $ext )
+        {
+            case '.gif':
+                imagegif($thumb, $outputFileName);
+                break;
+            case '.jpg':
+            case '.jpeg':
+                imagejpeg($thumb, $outputFileName, 80);
+                break;
+            case '.png':
+                imagepng($thumb, $outputFileName);
+                break;
+            case '.bmp':
+                imagewbmp($thumb, $outputFileName);
+                break;
+            default:
+                return false;
+        }
+
+
+        return true;
+    }
+
 
 ?><!doctype html>
 <html>
@@ -48,8 +126,11 @@ $(document).ready(function(){
 $myDirectory = opendir("images");
 while($entryName = readdir($myDirectory)) {
 	if($entryName != '.' && $entryName != '..'){
+	if(!is_file('thumbs/'.$entryName)){
+		thumbnail('images/'.$entryName, 'thumbs/'.$entryName ,140);
+	}
 ?>
-	<a href="images/<?php echo $entryName;?>" class="lightbox thumblink" title="<?php echo getTitleFromFileName($entryName); ?>"><img src="images/<?php echo $entryName; ?>" class="thumb" title="<?php echo getTitleFromFileName($entryName); ?>" /></a>
+	<a href="images/<?php echo $entryName;?>" class="lightbox thumblink" title="<?php echo getTitleFromFileName($entryName); ?>"><img src="thumbs/<?php echo $entryName; ?>" class="thumb" title="<?php echo getTitleFromFileName($entryName); ?>" /></a>
 <?php
 	}
 }
